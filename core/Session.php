4f3fea10f2a61a39d027a9dd27a2d0977348cad1
tmh3012 <78021876+tmh3012@users.noreply.github.com>
2023-05-28 05:10:52 +0700

    Create Session.php

---
+++ core/Session.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\core;
+
+class Session
+{
+    protected const FLASH_KEY = 'flash_message';
+
+    public function __construct()
+    {
+        session_start();
+        $flashMessages = $_SESSION[self::FLASH_KEY] ?? [];
+        foreach ($flashMessages as $key => &$flashMessage) {
+            // Mark remove message
+            // the message is only visible during the current request. On the next request, the message has been removed.
+            $flashMessage['removed'] = true;
+        }
+        $_SESSION[self::FLASH_KEY] = $flashMessages;
+    }
+
+    public function setFlash($key, $message)
+    {
+        $_SESSION[self::FLASH_KEY][$key] = [
+            'removed' => false,
+            'message' => $message
+        ];
+    }
+
+    public function getFlash($key)
+    {
+        return $_SESSION[self::FLASH_KEY][$key]['message'] ?? false;
+    }
+
+    public function __destruct()
+    {
+        $flashMessages = $_SESSION[self::FLASH_KEY] ?? [];
+        foreach ($flashMessages as $key => &$flashMessage) {
+            if ( $flashMessage['removed']) {
+                unset($flashMessages[$key]);
+            }
+        }
+        $_SESSION[self::FLASH_KEY] = $flashMessages;
+    }
+}
