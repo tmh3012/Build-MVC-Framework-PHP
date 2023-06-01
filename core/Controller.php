0d0cfc7749718978f7ca588472a0c21b4370ac51
tmh3012 <78021876+tmh3012@users.noreply.github.com>
2023-06-02 00:52:45 +0700

    Update Controller.php
    add method registerMiddleware($middleware)

--- core/Controller.php
+++ core/Controller.php
@@ -2,9 +2,18 @@
 
 namespace app\core;
 
+use app\core\middleware\BaseMiddleware;
+
 class Controller
 {
     public string $layout = "master";
+    public string $actions = "";
+
+    /**
+     * @var BaseMiddleware[]
+    */
+
+    public  array $middleware = [];
 
     /**
      * @param string
@@ -22,5 +31,15 @@ class Controller
     public function View($view, $data =[])
     {
         return Application::$app->view->renderView($view, $data);
+    }
+
+    public function registerMiddleware(BaseMiddleware $middleware)
+    {
+        $this->middleware[] = $middleware;
+    }
+
+    public function getMiddlewares()
+    {
+        return $this->middleware;
     }
 }
