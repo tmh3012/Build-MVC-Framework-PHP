233c18e47d9a7db7df0a830b4afc7bda53129b02
tmh3012 <78021876+tmh3012@users.noreply.github.com>
2023-06-09 19:10:47 +0700

    Update login.php

--- views/Auth/login.php
+++ views/Auth/login.php
@@ -1,26 +1,35 @@
 <?php /**
  * @var $model \app\models\LoginForm
- */ ?>
+ */
+use app\core\Application;
+?>
 
 <div class="main-container">
     <div class="background">
         <div class="background-image"
              style="background-image: url('https://down-vn.img.susercontent.com/file/sg-11134004-7qvd5-lfuyd509f57p08')">
-            <?php $form = \app\core\form\Form::begin('form-auth', 'form-auth', '', 'POST') ?>
-            <div class="form-head">
-                <h2 class="form-title">Login</h2>
-            </div>
-            <div class="form-body">
-                <?php echo $form->field($model,'email', 'Your email address')->type('email') ?>
-                <?php echo $form->field($model,'password','Enter your password')->type('password') ?>
-                <div class="form-group">
-                    <button class="btn btn--primary">Submit</button>
+            <div class="auth-content">
+                <?php if(Application::$app->session->getFlash('auth_error')): ?>
+                    <div class="error-message">
+                        <span><?php echo Application::$app->session->getFlash('auth_error') ?></span>
+                    </div>
+                <?php endif; ?>
+                <?php $form = \app\core\form\Form::begin('form-auth', 'form-auth', '', 'POST') ?>
+                <div class="form-head">
+                    <h2 class="form-title">Login</h2>
                 </div>
+                <div class="form-body">
+                    <?php echo $form->field($model,'email', 'Your email address')->type('email') ?>
+                    <?php echo $form->field($model,'password','Enter your password')->type('password') ?>
+                    <div class="form-group">
+                        <button class="btn btn--primary">Submit</button>
+                    </div>
+                </div>
+                <div class="form-footer">
+                    <p class="description">You don't have an account ?<a href="/register"> Register here</a> </p>
+                </div>
+                <?php echo \app\core\form\Form::end() ?>
             </div>
-            <div class="form-footer">
-                <p class="description">You don't have an account ?<a href="/register"> Register here</a> </p>
-            </div>
-            <?php echo \app\core\form\Form::end() ?>
         </div>
     </div>
 </div>
